dfb1870d2565e7adfac0c762aab377d8a9e26355
pegasys <sconnolly7@outlook.com>
2024-04-13 11:13:39 +0100

    added minigame base script + folders

---
+++ Assets/Scripts/Minigame/MinigameBase.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public enum MinigameScore
+{
+    None,
+    Bronze,
+    Silver,
+    Gold,
+    Platinum
+}
+
+public abstract class MinigameBase : MonoBehaviour
+{
+    /// <summary>
+    /// Thrown when the minigame is over, gives the score achieved
+    /// </summary>
+    public event EventHandler<MinigameScore> OnMinigameOver;
+
+    /// <summary>
+    /// Starts the minigame
+    /// </summary>
+    /// <returns>Returns true if started successfully</returns>
+    public abstract bool StartMinigame();
+}
