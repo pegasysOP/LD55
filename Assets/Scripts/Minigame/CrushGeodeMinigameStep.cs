b8ed65a4608a66f2001b2f071b9239df0d54ba71
LGNDLeviathan <roitchie@gmail.com>
2024-04-13 16:24:30 +0100

    Added partially implemented CrushGeodeMinigameStep

---
+++ Assets/Scripts/Minigame/CrushGeodeMinigameStep.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrushGeodeMinigameStep : MinigameStep
+{
+    public override event EventHandler<MinigameScore> OnMinigameStepOver;
+
+    public override bool StartMinigameStep()
+    {
+        Debug.Log("Crush Geode Minigame step started");
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Crush Geode Minigame step complete");
+        OnMinigameStepOver.Invoke(this, MinigameScore.None);
+    }
+}
--- Assets/Scripts/Minigame/GolemMinigame.cs
+++ Assets/Scripts/Minigame/GolemMinigame.cs
@@ -8,29 +8,42 @@ public class GolemMinigame : Minigame
 {
     public override event EventHandler<MinigameScore> OnMinigameOver;
 
+    private MinigameStep minigameStepInstance;
+
+
     public override bool StartMinigame()
     {
-        throw new System.NotImplementedException();
-
-        MinigameStep step;
+        Debug.Log("Start Minigame");
 
         //Get each minigame from the list of steps
-        while(steps.Count > 0)
-        {
-            step = steps[0];
-            steps.RemoveAt(0);
+        StartMinigameStep();
 
-            //Start each minigame step
-            step.StartMinigameStep();
+        return true;
+    }
 
-            //Wait for score from minigame step
+    public void StartMinigameStep()
+    {
+        //Get the first step 
+        MinigameStep step = steps[0];
+        steps.RemoveAt(0);
 
-            //Start next minigame step 
+        //Instantiate the step
+        minigameStepInstance = Instantiate(step);
 
-            //After all steps, calculate final score
+        //Add event listener for this step
+        step.OnMinigameStepOver += OnMinigameStepOver;
 
-            //Invoke OnMinigameOver and pass this final score back
-        }
+        //Start each minigame step
+        step.StartMinigameStep();
+
+
+        //Wait for score from minigame step
+
+        //Start next minigame step 
+
+        //After all steps, calculate final score
+
+        //Invoke OnMinigameOver and pass this final score back
     }
 
     // Start is called before the first frame update
@@ -42,7 +55,24 @@ public class GolemMinigame : Minigame
     // Update is called once per frame
     void Update()
     {
-            //End minigame
-            OnMinigameOver.Invoke(this, MinigameScore.Bronze);
+        //End minigame
+       // OnMinigameOver.Invoke(this, MinigameScore.Bronze);
+        
     }
+
+    private void OnMinigameStepOver(object sender, MinigameScore score)
+    {
+        if(steps.Count == 0)
+        {
+            Destroy(minigameStepInstance);
+            Debug.Log("Minigame over");
+            //There are no more steps so end the minigame 
+            OnMinigameOver.Invoke(this, score);
+        }
+
+        //If there are more steps then get the next step and start it 
+        StartMinigameStep();
+    }
+
+
 }
