3ef7ec346e5a8deac40a3218164277a65a4c84a0
LGNDLeviathan <roitchie@gmail.com>
2024-04-13 19:17:37 +0100

    Added slider mechanics for furnace minigame

--- Assets/Scripts/Minigame/Golem/Steps/FurnaceMinigameStep.cs
+++ Assets/Scripts/Minigame/Golem/Steps/FurnaceMinigameStep.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FurnaceMinigameStep : MinigameStep
 {
     public override event EventHandler<MinigameScore> OnMinigameStepOver;
+
+    public Slider furnaceHeatSlider;
+
+    private float timerDuration = 10f;
+    private float timer;
 
     public override bool StartMinigameStep()
     {
@@ -16,16 +22,53 @@ public class FurnaceMinigameStep : MinigameStep
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = timerDuration;
+        furnaceHeatSlider.value = 7;
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        Debug.Log(furnaceHeatSlider.value);
+        furnaceHeatSlider.value -= Time.deltaTime;
+
+        if (furnaceHeatSlider.value == 0 || furnaceHeatSlider.value > furnaceHeatSlider.maxValue - 0.1)
+        {
+            //Minigame should fail if the heat completely dissapears or reaches the maximum
+            OnMinigameStepOver.Invoke(this, MinigameScore.None);
+        }
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            //Get the sliders value and score accordingly
+            OnMinigameStepOver.Invoke(this, MinigameScore.Bronze);
+        }
+        HandleInput();
+        
+    }
+
+    void HandleInput()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Furnace Stoke Minigame step complete");
             OnMinigameStepOver.Invoke(this, MinigameScore.Silver);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left mouse button pressed");
+            if (furnaceHeatSlider.value + 1 < furnaceHeatSlider.maxValue)
+            {
+                furnaceHeatSlider.value += 1;
+            }
+            else
+            {
+                furnaceHeatSlider.value = furnaceHeatSlider.maxValue;
+            }
+
         }
     }
 }
