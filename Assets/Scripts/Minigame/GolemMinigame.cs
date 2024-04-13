196d109e4b7d9400f37deb3cef5ea0a8a597f96c
LGNDLeviathan <roitchie@gmail.com>
2024-04-13 12:56:22 +0100

    Add steps for minigame

--- Assets/Scripts/Minigame/GolemMinigame.cs
+++ Assets/Scripts/Minigame/GolemMinigame.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GolemMinigame : Minigame
 {
@@ -10,17 +11,26 @@ public class GolemMinigame : Minigame
     public override bool StartMinigame()
     {
         throw new System.NotImplementedException();
+
+        MinigameStep step;
+
         //Get each minigame from the list of steps
+        while(steps.Count > 0)
+        {
+            step = steps[0];
+            steps.RemoveAt(0);
 
-        //Start each minigame step
+            //Start each minigame step
+            step.StartMinigameStep();
 
-        //Wait for score from minigame step
+            //Wait for score from minigame step
 
-        //Start next minigame step 
+            //Start next minigame step 
 
-        //After all steps, calculate final score
+            //After all steps, calculate final score
 
-        //Invoke OnMinigameOver and pass this final score back
+            //Invoke OnMinigameOver and pass this final score back
+        }
     }
 
     // Start is called before the first frame update
