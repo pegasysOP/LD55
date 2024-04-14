d3b48a815f7c568ce324922a6e3b83e704d420ee
LGNDLeviathan <roitchie@gmail.com>
2024-04-14 17:00:37 +0100

    Added timer class to sprinkle powder minigame

--- Assets/Scripts/Minigame/Phoenix/Steps/SprinklePowderMinigameStep.cs
+++ Assets/Scripts/Minigame/Phoenix/Steps/SprinklePowderMinigameStep.cs
@@ -9,7 +9,8 @@ public class SprinklePowderMinigameStep : MinigameStep
     public override event EventHandler<MedalType> OnMinigameStepOver;
 
     private float timerDuration = 10f;
-    private float timer;
+
+    [SerializeField] Timer timer;
 
     [SerializeField] GameObject TimerGO;
 
@@ -18,41 +19,35 @@ public class SprinklePowderMinigameStep : MinigameStep
     public override bool StartMinigameStep()
     {
         Debug.Log("Sprinkle Powder Minigame step started");
+        timer.StartTimer(timerDuration, OnTimerFinished);
         return true;
+    }
+
+    private void OnTimerFinished()
+    {
+        DestroyPowder();
+        OnMinigameStepOver.Invoke(this, MedalType.None);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        timer = timerDuration;
-        TimerGO.GetComponent<Slider>().maxValue = timerDuration;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        timer -= Time.deltaTime;
-        TimerGO.GetComponent<Slider>().value = Mathf.CeilToInt(timer);
+    {   
+        HandleInput();
+    }
 
+    void HandleInput()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             DestroyPowder();
             Debug.Log("Sprinkle Powder Minigame step complete");
             OnMinigameStepOver.Invoke(this, MedalType.Bronze);
         }
-
-        if (timer <= 0)
-        {
-            DestroyPowder();
-            OnMinigameStepOver.Invoke(this, MedalType.None);
-        }
-
-        HandleInput();
-
-    }
-
-    void HandleInput()
-    {
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Mouse input detected");
