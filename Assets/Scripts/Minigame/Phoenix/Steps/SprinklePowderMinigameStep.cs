2d23494b6222a5d69ca09dfba15ffba8ce3b29f4
LGNDLeviathan <roitchie@gmail.com>
2024-04-14 15:27:55 +0100

    Added timer and loss condition to sprinkle powder minigame

--- Assets/Scripts/Minigame/Phoenix/Steps/SprinklePowderMinigameStep.cs
+++ Assets/Scripts/Minigame/Phoenix/Steps/SprinklePowderMinigameStep.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SprinklePowderMinigameStep : MinigameStep
 {
     public override event EventHandler<MedalType> OnMinigameStepOver;
+
+    private float timerDuration = 10f;
+    private float timer;
+
+    [SerializeField] GameObject TimerGO;
 
     public override bool StartMinigameStep()
     {
@@ -16,16 +22,25 @@ public class SprinklePowderMinigameStep : MinigameStep
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = timerDuration;
+        TimerGO.GetComponent<Slider>().maxValue = timerDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+        TimerGO.GetComponent<Slider>().value = Mathf.CeilToInt(timer);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Sprinkle Powder Minigame step complete");
             OnMinigameStepOver.Invoke(this, MedalType.Bronze);
+        }
+
+        if (timer <= 0)
+        {
+            OnMinigameStepOver.Invoke(this, MedalType.None);
         }
     }
 }
