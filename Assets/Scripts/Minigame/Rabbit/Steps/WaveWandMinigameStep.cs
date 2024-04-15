bd00875bdf8b0dfafb3ff4554f4f0d01188aa1dc
LGNDLeviathan <roitchie@gmail.com>
2024-04-15 17:05:00 +0100

    Paint is now always destroyed

--- Assets/Scripts/Minigame/Rabbit/Steps/WaveWandMinigameStep.cs
+++ Assets/Scripts/Minigame/Rabbit/Steps/WaveWandMinigameStep.cs
@@ -13,6 +13,13 @@ public class WaveWandMinigameStep : MinigameStep
 
     public GameObject paintSplodge;
 
+    [SerializeField] GameObject[] checkpoints;
+
+    GameObject[] checkpointsLeft;
+
+    private int numCheckpoints;
+    private int checkPointsHit;
+
     public override bool StartMinigameStep()
     {
         Debug.Log("Wave wand minigame started");
@@ -22,18 +29,24 @@ public class WaveWandMinigameStep : MinigameStep
 
     void OnTimerFinished()
     {
+        DestroyPaint();
+        OnMinigameStepOver.Invoke(this, MedalType.None);
+    }
+
+    void DestroyPaint()
+    {
         GameObject[] toDestroy = GameObject.FindGameObjectsWithTag("Paint");
         foreach (GameObject go in toDestroy)
         {
             Destroy(go);
         }
-        OnMinigameStepOver.Invoke(this, MedalType.None);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        numCheckpoints = checkpoints.Length;
+        checkpointsLeft = checkpoints;
     }
 
     // Update is called once per frame
@@ -50,7 +63,33 @@ public class WaveWandMinigameStep : MinigameStep
         if (isDragging)
         {
             Draw(Input.mousePosition);
+            GameObject go = HitCheckpoint(Input.mousePosition);
+            if(go != null)
+            {
+
+                if(go.tag == "Checkpoint1")
+                {
+
+                }
+
+                for(int i = 0; i < checkpointsLeft.Length; i++)
+                {
+                    if (go == checkpointsLeft[i])
+                    {
+                        checkpointsLeft[i] = null;
+                    }
+                }
+                checkPointsHit += 1;
+                go = null;
+            }
         }
+
+        if(checkPointsHit == numCheckpoints)
+        {
+            DestroyPaint();
+            OnMinigameStepOver.Invoke(this, MedalType.Jade);
+        }
+        
     }
 
     void Draw(Vector2 mousePosition)
@@ -71,5 +110,26 @@ public class WaveWandMinigameStep : MinigameStep
         {
             isDragging = false;
         }
+    }
+
+    private GameObject HitCheckpoint(Vector2 mousePos)
+    {
+        // Get the rectTransform component of the chili image
+        //RectTransform rectTransform = chilliGO.rectTransform;
+
+        // Create a ray from the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        // Raycast against the chili image
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+        // Check if the raycast hit the petal
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit checkpoint");
+            return hit.collider.gameObject;
+        }
+
+        return null;
     }
 }
