843d0084e3a99632d436b4ef29779205f1d30495
LGNDLeviathan <roitchie@gmail.com>
2024-04-15 15:45:54 +0100

    Added drawing for wave wand minigame

--- Assets/Scripts/Minigame/Rabbit/Steps/WaveWandMinigameStep.cs
+++ Assets/Scripts/Minigame/Rabbit/Steps/WaveWandMinigameStep.cs
@@ -9,6 +9,10 @@ public class WaveWandMinigameStep : MinigameStep
     [SerializeField] Timer timer;
     float timerDuration = 10f;
 
+    bool isDragging = false;
+
+    public GameObject paintSplodge;
+
     public override bool StartMinigameStep()
     {
         Debug.Log("Wave wand minigame started");
@@ -18,6 +22,11 @@ public class WaveWandMinigameStep : MinigameStep
 
     void OnTimerFinished()
     {
+        GameObject[] toDestroy = GameObject.FindGameObjectsWithTag("Paint");
+        foreach (GameObject go in toDestroy)
+        {
+            Destroy(go);
+        }
         OnMinigameStepOver.Invoke(this, MedalType.None);
     }
 
@@ -34,6 +43,33 @@ public class WaveWandMinigameStep : MinigameStep
         {
             Debug.Log("Wave wand Minigame step complete");
             OnMinigameStepOver.Invoke(this, MedalType.Bronze);
+        }
+
+        HandleInput();
+
+        if (isDragging)
+        {
+            Draw(Input.mousePosition);
+        }
+    }
+
+    void Draw(Vector2 mousePosition)
+    {
+        Vector3 convertedMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        convertedMousePosition.z = 0;
+
+        Instantiate(paintSplodge, convertedMousePosition , Quaternion.identity);
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
         }
     }
 }
