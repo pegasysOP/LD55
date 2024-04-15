967c48edc29074f8969781f07c35676abccb284e
LGNDLeviathan <roitchie@gmail.com>
2024-04-15 11:59:25 +0100

    Implemented petal pluck minigame

--- Assets/Scripts/Minigame/Rabbit/Steps/PluckFlowerMinigameStep.cs
+++ Assets/Scripts/Minigame/Rabbit/Steps/PluckFlowerMinigameStep.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PluckFlowerMinigameStep : MinigameStep
@@ -9,10 +10,20 @@ public class PluckFlowerMinigameStep : MinigameStep
     
     [SerializeField] Timer timer;
     float timerDuration = 10f;
+
+    [SerializeField] GameObject[] petals;
+
+    GameObject petal = null;
+
+    bool isDragging = false;
+
+    int petalsToPluck;
+
     public override bool StartMinigameStep()
     {
         Debug.Log("Pluck flower minigame started");
         timer.StartTimer(timerDuration, OnTimerFinished);
+        petalsToPluck = petals.Length;
         return true;
     }
 
@@ -35,5 +46,89 @@ public class PluckFlowerMinigameStep : MinigameStep
             Debug.Log("Pluck Flower Minigame step complete");
             OnMinigameStepOver.Invoke(this, MedalType.Bronze);
         }
+
+        HandleInput();
+
+        if (isDragging)
+        {
+            if (HitPetal(Input.mousePosition))
+            {
+                petal = DragPetal(Input.mousePosition);
+                petal.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+            }
+        }
+        else
+        {
+            
+            if(petal != null)
+            {
+                Debug.Log("Petal: " + petal.name);
+                petal.AddComponent<Rigidbody2D>();
+                petal.GetComponent<Rigidbody2D>().gravityScale = 0.25f;
+                petalsToPluck -= 1;
+                petal = null;
+            }
+
+        }
+
+        if(petalsToPluck == 0)
+        {
+            OnMinigameStepOver.Invoke(this, MedalType.Jade);
+        }
+
+    }
+
+
+    void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+    }
+
+    private bool HitPetal(Vector2 mousePos)
+    {
+        // Get the rectTransform component of the chili image
+        //RectTransform rectTransform = chilliGO.rectTransform;
+
+        // Create a ray from the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        // Raycast against the chili image
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+        // Check if the raycast hit the petal
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private GameObject DragPetal(Vector2 mousePos)
+    {
+        // Get the rectTransform component of the chili image
+        //RectTransform rectTransform = chilliGO.rectTransform;
+
+        // Create a ray from the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        // Raycast against the chili image
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+
+        // Check if the raycast hit the petal
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject;
+        }
+
+        return null;
     }
 }
